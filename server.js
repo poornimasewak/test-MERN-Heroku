c51d7e23f96a5568a99a2aac20174614eb8c7823
poornima sewak <poornima.mathur.20@gmail.com>
2018-04-21 11:37:26 -0500

    add on heroku

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8000;
+var app = express();
+// ... other imports
+const path = require("path")
+
+// ... other app.use middleware setups
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+
+
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {  
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
+});
