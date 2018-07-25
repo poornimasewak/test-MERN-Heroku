954661e7297486f3485c5a97fa7de390f782b9f8
poornima sewak <poornima.mathur.20@gmail.com>
2018-07-25 11:39:04 -0500

    delete

--- client/src/Constants/routes.js
+++
@@ -1,6 +0,0 @@
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/signin';
-export const HOME = '/';
-export const ACCOUNT = '/account';
-export const PASSWORD_FORGET = '/passwordforget';
-// export const SIGN_UP = '/signup';
